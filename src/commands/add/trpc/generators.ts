83609bebf2312fc4c78484409a875db7e62648d6
Nico Albanese <gcalbanese96@gmail.com>
2023-08-31 16:55:58 +0100

    updated trpc config to move to server invoke mode

--- src/commands/add/trpc/generators.ts
+++ src/commands/add/trpc/generators.ts
@@ -15,12 +15,29 @@ export type AppRouter = typeof appRouter;
 export const serverTrpcTs = () => {
   return `import { initTRPC, TRPCError } from "@trpc/server";
 import { Context } from "../trpc/context";
+import superjson from "superjson";
+import { ZodError } from "zod";
 
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter(opts) {
+    const { shape, error } = opts;
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  },
+});
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
@@ -30,11 +47,11 @@ export const publicProcedure = t.procedure;`;
 };
 
 // 3. create server/router/users.ts directory and maybe a users file
-export const serverRouterUsersTs = () => {
+export const serverRouterComputersTs = () => {
   return `import { publicProcedure, router } from "../trpc";
-export const usersRouter = router({
-  getUsers: publicProcedure.query(async () => {
-    return [{ id: 1, name: "John Doe" }, { id: 2, name: "Jane Doe" }];
+export const computersRouter = router({
+  getComputers: publicProcedure.query(async () => {
+    return [{ id: 1, name: "Macintosh" }, { id: 2, name: "Microsoft" }];
   }),
 });
 `;
@@ -75,14 +92,17 @@ import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
 
 import { trpc } from "./client";
+import { getUrl } from "./utils";
+import SuperJSON from "superjson";
 
 export default function TrpcProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
     trpc.createClient({
+      transformer: SuperJSON,
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: getUrl(),
         }),
       ],
     })
@@ -111,6 +131,48 @@ export const serverClient = appRouter.createCaller({
 `;
 };
 
+export const libTrpcApiTs = () => {
+  return `import { cookies } from "next/headers";
+import { getUserAuth } from "../auth/utils";
+import { appRouter } from "../server/routers/_app";
+import { loggerLink } from "@trpc/client";
+import { experimental_createTRPCNextAppDirServer as createTRPCNextAppDirServer } from "@trpc/next/app-dir/server";
+import { experimental_nextCacheLink as nextCacheLink } from "@trpc/next/app-dir/links/nextCache";
+import SuperJSON from "superjson";
+
+/**
+ * This client invokes procedures directly on the server without fetching over HTTP.
+ */
+export const api = createTRPCNextAppDirServer<typeof appRouter>({
+  config() {
+    return {
+      transformer: SuperJSON,
+      links: [
+        loggerLink({
+          enabled: (op) => true,
+        }),
+        nextCacheLink({
+          // requests are cached for 5 seconds
+          revalidate: 5,
+          router: appRouter,
+          async createContext() {
+            const { session } = await getUserAuth();
+            return {
+              session,
+              headers: {
+                cookie: cookies().toString(),
+                "x-trpc-source": "rsc-invoke",
+              },
+            };
+          },
+        }),
+      ],
+    };
+  },
+});
+`;
+};
+
 // 8. create lib/trpc/context.ts
 export const libTrpcContextTs = (withSession: boolean = false) => {
   return `import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
@@ -127,4 +189,16 @@ ${withSession ? "" : " // "}const { session } = await getUserAuth();
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
 `;
+};
+
+export const libTrpcUtilsTs = () => {
+  return `function getBaseUrl() {
+  if (typeof window !== "undefined") return "";
+  if (process.env.VERCEL_URL) return \`https://${process.env.VERCEL_URL}\`;
+  return "http://localhost:3000";
+}
+
+export function getUrl() {
+  return getBaseUrl() + "/api/trpc";
+}`;
 };
--- src/commands/add/trpc/index.ts
+++ src/commands/add/trpc/index.ts
@@ -7,12 +7,14 @@ import {
 } from "../../../utils.js";
 import {
   apiTrpcRouteTs,
+  libTrpcApiTs,
   libTrpcClientTs,
   libTrpcContextTs,
   libTrpcProviderTsx,
   libTrpcServerClientTs,
+  libTrpcUtilsTs,
   rootRouterTs,
-  serverRouterUsersTs,
+  serverRouterComputersTs,
   serverTrpcTs,
 } from "./generators.js";
 import { addContextProviderToLayout } from "../utils.js";
@@ -26,15 +28,19 @@ export const addTrpc = async () => {
   // 2. create lib/server/trpc.ts
   createFile(`${rootPath}/lib/server/trpc.ts`, serverTrpcTs());
   // 3. create lib/server/router/ directory and maybe a users file
-  createFile(`${rootPath}/lib/server/routers/users.ts`, serverRouterUsersTs());
+  createFile(
+    `${rootPath}/lib/server/routers/computers.ts`,
+    serverRouterComputersTs()
+  );
   // 4. create app/api/trpc/[trpc]/route.ts
   createFile(`${rootPath}/app/api/trpc/[trpc]/route.ts`, apiTrpcRouteTs());
   // 5. create lib/trpc/client.ts
   createFile(`${rootPath}/lib/trpc/client.ts`, libTrpcClientTs());
   // 6. create lib/trpc/Provider.tsx
   createFile(`${rootPath}/lib/trpc/Provider.tsx`, libTrpcProviderTsx());
-  // 7. create lib/trpc/serverClient.ts
-  createFile(`${rootPath}/lib/trpc/serverClient.ts`, libTrpcServerClientTs());
+  // 7. create lib/trpc/serverClient.ts -> updated to lib/trpc/api.ts using server invoker
+  // createFile(`${rootPath}/lib/trpc/serverClient.ts`, libTrpcServerClientTs());
+  createFile(`${rootPath}/lib/trpc/api.ts`, libTrpcApiTs());
 
   // 7.5. create context file and update to include context file above
   createFile(
@@ -42,11 +48,14 @@ export const addTrpc = async () => {
     libTrpcContextTs(packages.includes("next-auth"))
   );
 
+  // create trpc utils file lib/trpc/utils.ts
+  createFile(`${rootPath}/lib/trpc/utils.ts`, libTrpcUtilsTs());
+
   // 8. Install Packages: @tanstack/react-query, @trpc/client, @trpc/react-query, @trpc/server
   await installPackages(
     {
       regular:
-        "@tanstack/react-query @trpc/client @trpc/react-query @trpc/server",
+        "@tanstack/react-query @trpc/client @trpc/react-query @trpc/server @trpc/next superjson",
       dev: "",
     },
     preferredPackageManager
