2bcd45fa86873ffe4342ace63a7d040294f3643c
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-10-06 13:22:24 +0300

    allow init options for add command

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,14 @@ program
 program
   .command("add")
   .description("Add and setup additional packages")
+  .option("-sf, --has-src-folder <has>", "has a src folder")
+  .option("-pm, --package-manager <pm>", "preferred package manager")
+  .option("-o, --orm <orm>", "preferred orm")
+  .option("-db, --db <db>", "preferred database")
+  .option("-a, --auth <auth>", "preferred auth")
+  .option("-ap, --auth-providers <auth-providers...>", "auth providers")
+  .option("-p, --packages <packages...>", "packages")
+  .option("-ie, --include-example <include>", "include example")
   .action(addPackage);
 
 program
