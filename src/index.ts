8ce5e1a3f9ebe07d3340bd8e0fd80b132921720a
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-10-06 13:26:53 +0300

    avoid copy paste of options

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,8 @@ import { updateConfigFileAfterUpdate } from "./utils.js";
 const program = new Command();
 program.name("kirimase").description("Kirimase CLI").version("0.0.17");
 
-program
-  .command("init")
+addCommonOptions(program.command("init"))
   .description("initialise and configure kirimase within directory")
-  .option("-sf, --has-src-folder <has>", "has a src folder")
-  .option("-pm, --package-manager <pm>", "preferred package manager")
-  .option("-o, --orm <orm>", "preferred orm")
-  .option("-db, --db <db>", "preferred database")
-  .option("-a, --auth <auth>", "preferred auth")
-  .option("-ap, --auth-providers <auth-providers...>", "auth providers")
-  .option("-p, --packages <packages...>", "packages")
-  .option("-ie, --include-example <include>", "include example")
   .action(initProject);
 
 program
@@ -27,17 +18,8 @@ program
   .description("Generate a new resource")
   .action(buildSchema);
 
-program
-  .command("add")
+addCommonOptions(program.command("add"))
   .description("Add and setup additional packages")
-  .option("-sf, --has-src-folder <has>", "has a src folder")
-  .option("-pm, --package-manager <pm>", "preferred package manager")
-  .option("-o, --orm <orm>", "preferred orm")
-  .option("-db, --db <db>", "preferred database")
-  .option("-a, --auth <auth>", "preferred auth")
-  .option("-ap, --auth-providers <auth-providers...>", "auth providers")
-  .option("-p, --packages <packages...>", "packages")
-  .option("-ie, --include-example <include>", "include example")
   .action(addPackage);
 
 program
@@ -46,3 +28,15 @@ program
   .action(updateConfigFileAfterUpdate);
 
 program.parse(process.argv);
+
+function addCommonOptions(command: Command) {
+  return command
+    .option("-sf, --has-src-folder <has>", "has a src folder")
+    .option("-pm, --package-manager <pm>", "preferred package manager")
+    .option("-o, --orm <orm>", "preferred orm")
+    .option("-db, --db <db>", "preferred database")
+    .option("-a, --auth <auth>", "preferred auth")
+    .option("-ap, --auth-providers <auth-providers...>", "auth providers")
+    .option("-p, --packages <packages...>", "packages")
+    .option("-ie, --include-example <include>", "include example");
+}
