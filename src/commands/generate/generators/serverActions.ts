9f1e159f339d4fc35140af762a3a90accb7d98ed
Nico Albanese <gcalbanese96@gmail.com>
2024-01-12 14:25:25 +0000

    fixes issue with error handling within service fns and actions

--- src/commands/generate/generators/serverActions.ts
+++ src/commands/generate/generators/serverActions.ts
@@ -47,9 +47,13 @@ import {
   })}";
 
 const handleErrors = (e: unknown) => {
-  if (e instanceof Error) return e.message;
-  if (e && typeof e === "object" && "error" in e) return e.error as string;
-  return "Error";
+  const errMsg = "Error, please try again.";
+  if (e instanceof Error) return e.message.length > 0 ? e.message : errMsg;
+  if (e && typeof e === "object" && "error" in e) {
+    const errAsStr = e.error as string;
+    return errAsStr.length > 0 ? errAsStr : errMsg;
+  }
+  return errMsg;
 };
 
 const revalidate${tableNamePluralCapitalised} = () => revalidatePath("/${tableNameKebabCase}");
