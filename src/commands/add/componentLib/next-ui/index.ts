ce66cbd9529ae295cdb76a1ecc5f576bcbedd8e6
Affan Siddqiui <siddiquiaffan201@gmail.com>
2024-02-14 06:45:40 +0530

    rename to themeToggler

--- src/commands/add/componentLib/next-ui/index.ts
+++ src/commands/add/componentLib/next-ui/index.ts
@@ -8,6 +8,7 @@ import {
     addPackageToConfig,
     createFile,
     readConfigFile,
+    replaceFile,
     updateConfigFile,
     // replaceFile,
 } from "../../../../utils.js";
@@ -15,7 +16,7 @@ import {
     addContextProviderToRootLayout,
     addToInstallList,
 } from "../../utils.js";
-import { generateNextUIWithThemeProvider, generateTailwindConfigWithNextUIContent, generateThemeSwitcher } from "./generators.js";
+import { generateNextUIWithThemeProvider, generateTailwindConfigWithNextUIContent, generateThemeToggler } from "./generators.js";
 
 export const installNextUI = async () => {
     const {
@@ -39,7 +40,7 @@ export const installNextUI = async () => {
     // generate themeswitcher component
     createFile(
         rootPath.concat("components/ui/ThemeSwitcher.tsx"),
-        generateThemeSwitcher()
+        generateThemeToggler()
     )
 
     // wrap root layout with next ui provider
@@ -49,3 +50,33 @@ export const installNextUI = async () => {
     updateConfigFile({ componentLib: "next-ui" });
 
 }
+
+export const updateSignInComponentWithNextUI = () => {
+    const { hasSrc, alias } = readConfigFile();
+    const filepath = "components/auth/SignIn.tsx";
+    const updatedContent = `"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+import {Button} from "@nextui-org/react";
+
+export default function SignIn() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <div>Loading...</div>;
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <Button variant="flat" color="danger" onClick={() => signOut({ callbackUrl: "/" })}>Sign out</Button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <Button onClick={() => signIn()}>Sign in</Button>
+    </>
+  );
+}`;
+    replaceFile(`${hasSrc ? "src/" : ""}${filepath}`, updatedContent);
+};
