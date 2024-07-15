8a2033be07b64318b15a876d2f6927de19e1e0d1
Nico Albanese <nico.albanese@vercel.com>
2024-07-15 14:28:40 +0100

    update stripe api date

--- src/commands/add/misc/stripe/generators.ts
+++ src/commands/add/misc/stripe/generators.ts
@@ -11,7 +11,7 @@ export const generateStripeIndexTs = () => {
   return `import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-06-20",
   typescript: true,
 });
 `;
