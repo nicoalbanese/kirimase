aeb2882ff5cde98cbfda3836a3b5d94c293dd0ec
FADHILI Josue <fadhilijosue27@gmail.com>
2024-08-07 16:21:28 +0200

    chore: sync kirimase version with package.json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings=ExperimentalWarning
 
 import { Command } from "commander";
 import { initProject } from "./commands/init/index.js";
 import { buildSchema } from "./commands/generate/index.js";
 import { addPackage } from "./commands/add/index.js";
 import { toggleAnalytics } from "./commands/init/utils.js";
+import pkg from '../package.json' assert { type: "json" };
 
 const program = new Command();
-program.name("kirimase").description("Kirimase CLI").version("0.0.59");
+program.name("kirimase").description("Kirimase CLI").version(
+  `\u001B[1mv${pkg.version}\u001B[0m`,
+  '-v, --version',
+  "Kirimase version.",
+).helpOption('-h, --help', 'Output usage of Kirimase.');
 
 program
   .command("analytics")
